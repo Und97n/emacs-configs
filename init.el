2563655c017b1389e3d97d0790849a026242344a
ZizitopU <und97n@gmail.com>
2020-02-01 13:51:41 +0200

    Gitignore and cleanup in init.el

--- init.el
+++ init.el
@@ -115,17 +115,3 @@
 (require 'slime)
 (setq inferior-lisp-program "/usr/bin/sbcl")
 (setq slime-contribs '(slime-fancy))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (reverse-im tabbar highlight-symbol slime paredit gruvbox-theme auto-complete))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
